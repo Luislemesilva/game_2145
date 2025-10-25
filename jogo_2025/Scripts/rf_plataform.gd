4487936006bd9100be1df79dbaf767141b6bf8d9
Lanna <lannagreghiamaro@gmail.com>
2025-10-25 18:32:43 -0300

    Criação da fase do distrito tóxico e plataformas móveis

---
+++ jogo_2025/Scripts/rf_plataform.gd
@@ -0,0 +1,14 @@
+extends AnimatableBody2D
+
+@onready var target: Sprite2D = $Target
+
+func _ready() -> void:
+	
+	target.visible = false
+	
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(self, "global_position", target.global_position, 2)
+	tween.tween_property(self, "global_position", global_position, 2)
+
+	tween.set_loops()
